d29a6987f05cd53f53b14057ef49e9a31af57ce6
victor orsolan <vorsolan@hotmail.com>
2021-03-02 09:57:14 -0300

    refact user view to show created account

--- lib/rocketpay_web/views/user_view.ex
+++ lib/rocketpay_web/views/user_view.ex
@@ -1,13 +1,20 @@
 defmodule RocketpayWeb.UsersView do
-  alias Rocketpay.User
+  alias Rocketpay.{Account, User}
 
-  def rednder("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+        account: %Account{id: account_id, balance: balance},
+        user: %User{id: id, name: name, nickname: nickname}
+      }) do
     %{
       message: "User created!",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
